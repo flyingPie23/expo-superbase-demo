759de0eed8350dc0a43842b69b21fa9a76d492ac
flyingPie23 <drissfreah325@gmail.com>
2025-10-02 12:25:06 +0100

    added expo rapid UI

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from "react-native-rapi-ui";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme="light">
+      <Text style="" onPress={() => window.location.reload()}>Test</Text>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
